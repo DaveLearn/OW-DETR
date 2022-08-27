b0a634d1956afe9df414e8957b533112aac205da
David Pershouse <d3.pershouse@hdr.qut.edu.au>
2022-08-27 14:47:36 +1000

    auto download dino backbone

--- deps.sh
+++ deps.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
+echo "Checking cuda ops"
 if [[ ! $(pip freeze | grep -i MultiScaleDeformableAttention) ]]; then
   echo "Installing cuda ops"
   pushd models/ops
   sh ./make.sh
   python test.py || exit 1
   popd
+fi
+
+echo "Checking dino backbone"
+if [[ ! -f models/dino_resnet50_pretrain.pth ]]; then
+  echo "Downloading pretrained dino backbone"
+  wget -O models/dino_resnet50_pretrain.pth https://dl.fbaipublicfiles.com/dino/dino_resnet50_pretrain/dino_resnet50_pretrain.pth || exit 1
 fi
