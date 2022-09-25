7d83e8bd4282b251a76fd6013cc700536681f6e6
David Pershouse <d3.pershouse@hdr.qut.edu.au>
2022-09-25 17:05:07 +1000

    learning rate for single gpu

--- configs/OWOD_our_proposed_split.sh
+++ configs/OWOD_our_proposed_split.sh
@@ -8,7 +8,7 @@ PY_ARGS=${@:1}
 python -u main_open_world.py \
     --output_dir ${EXP_DIR} --dataset owod --num_queries 100 --eval_every 5 \
     --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 19 --data_root './data/OWDETR' --train_set 't1_train' --test_set 'test' --num_classes 81 \
-    --unmatched_boxes --epochs 45 --top_unk 5 --featdim 1024 --NC_branch --nc_loss_coef 0.1 --nc_epoch 9 \
+    --unmatched_boxes --epochs 45 --lr 7e-05 --lr_backbone 7e-6 --top_unk 5 --featdim 1024 --NC_branch --nc_loss_coef 0.1 --nc_epoch 9 \
     --backbone 'dino_resnet50' \
     ${PY_ARGS}  2>&1 | tee -a ${EXP_DIR}/run_log.txt
 
