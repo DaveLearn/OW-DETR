ab4b0770d10a5011167d59047b73f79768a9b65a
David Pershouse <d3.pershouse@hdr.qut.edu.au>
2022-10-09 11:22:03 +1000

    scripts for full hpc run on 8 gpu

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-
-GPUS_PER_NODE=2 ./tools/run_dist_launch.sh 2 configs/OWOD_our_proposed_split.sh
+#!/bin/bash -l
+#PBS -N og_full
+#PBS -l ncpus=32,mem=256GB,ngpus=8,gputype=A100,walltime=192:00:00
+cd $PBS_O_WORKDIR
+conda activate owdetr || exit 1
+GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 configs/OWOD_our_proposed_split.sh
